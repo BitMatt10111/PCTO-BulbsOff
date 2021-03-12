775ff5511a8d08b3f2d5789a31d0d28c0861b173
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2021-03-12 16:12:44 +0100

    Update CursorCollide.cs

--- src/CursorCollide.cs
+++ src/CursorCollide.cs
@@ -13,7 +13,7 @@ public class CursorCollide : MonoBehaviour
 {
     //[SerializeField] Vector2 v;
     GameObject cursor;
-    
+    //here are all the variables that we use in the mission
     [SerializeField] GameObject imgB1;
     [SerializeField] GameObject imgB2;
     [SerializeField] GameObject imgR1;
@@ -34,21 +34,25 @@ public class CursorCollide : MonoBehaviour
     // public
     // public string IP = "127.0.0.1"; default local
     public int port=8051; // define > init 
-
+    
     void Start(){
         cursor = GameObject.Find("Puntatore");
         client = new UdpClient(port);
     }
 
     void FixedUpdate(){
+        //here I take the bytes of the socket sent by the python program in an array of bytes
         IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
         byte[] data = client.Receive(ref anyIP);
+        //in a float vector I take the bytes converted to float by the function:
         float[] floatData = ConvertByteToFloat(data);
+        //mapping the values for the main camera 
         float x=floatData[0]*10.5f/500;
         float y=floatData[1]*(-5.5f)/500;
         //print(x + "," + y);
 
         //v = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //set the cursor in the middle of the main camera and i check the five rounds of the game
         if(x == 0 && y == 0){
             cursor.transform.position= new Vector2(5,-3);
         }else{
@@ -63,11 +67,11 @@ public class CursorCollide : MonoBehaviour
         }
         //Debug.Log(v);
     }
-
+    
     void ChangeScene(){
         SceneManager.LoadScene(3);
     }
-
+    //in this function I check whether the colliders overlap, the cursor with the buttons. and I also increase the rounds
     void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("BlueBtn") || other.CompareTag("GreenBtn") || other.CompareTag("RedBtn") || other.CompareTag("YellowBtn")){
             if (other.CompareTag("BlueBtn")){
@@ -103,8 +107,9 @@ public class CursorCollide : MonoBehaviour
             rounds++;
         }
     }
-
+    //in this function I randomise the image of the mission
     void RandomImage(){
+        //I randomise a number from 1 to 8 in a variable that I control and according to the number I assign an image
         int numRand = (int) UnityEngine.Random.Range(1,9);
         if(numRand == 1 ){
             appoggio = imgB1;
@@ -131,9 +136,10 @@ public class CursorCollide : MonoBehaviour
             appoggio = imgG2;
             green=true;
         }
+        //when I have randomised the image I make it appear on the screen
         appoggio.GetComponent<SpriteRenderer>().enabled = true;
     }
-
+    //In this function you transform the bytes passed via parameter into floats, the function returns an array of floats
     public static float[] ConvertByteToFloat(byte[] array) {
         float[] floatArr = new float[array.Length / 4];
         for (int i = 0; i < floatArr.Length; i++) {
