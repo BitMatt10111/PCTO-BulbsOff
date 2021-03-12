a981d664ac19ca24090e7915b242a9f94967fe25
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2021-03-12 08:40:32 +0100

    Update Trigger.cs

--- src/Trigger.cs
+++ src/Trigger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+//These are the libraries used in unity
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,14 +7,15 @@ using UnityEngine.SceneManagement;
 
 public class Trigger : MonoBehaviour
 {
-
+    //this method is invoked when two objects with a hitbox overlap and the "ChangeScene" method is called.
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){
             ChangeScene();
         }
     }
-
+    //This method is used to change the scene
      void ChangeScene(){
+         //The LoadScene method is used to start a scene  
          SceneManager.LoadScene(2);
      }
      
