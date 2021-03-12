31406b7fb71c612704906ccc0d0b008908b4c913
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2021-03-12 08:47:49 +0100

    Update MenuScript.cs

--- src/MenuScript.cs
+++ src/MenuScript.cs
@@ -8,15 +8,17 @@ public class MenuScript : MonoBehaviour
 {
     [SerializeField] Image simbolsKey;
     [SerializeField] private bool ok=true;
-
+    
+    //these three methods are associated with buttons in the game menu
+    //The play method makes the scene change 
     public void Play(){
         ChangeScene();
     }
-
+    //this method closes the game
     public void Quit(){
         Application.Quit();
     }
-
+    //this method is used to make an image appear in the game if pressed and remove it if pressed again
     public void Rules(){
          ok=!ok;
         simbolsKey.enabled=ok;
