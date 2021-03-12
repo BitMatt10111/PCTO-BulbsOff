db45437b6929d988297b0a376d21073ee3b3d962
Matteo Lamberti <72201009+BitMatt10111@users.noreply.github.com>
2021-03-12 15:47:26 +0100

    Update CameraBehaviour.cs

--- src/CameraBehaviour.cs
+++ src/CameraBehaviour.cs
@@ -6,9 +6,9 @@ public class CameraBehaviour : MonoBehaviour
 {
     Transform player;
     [SerializeField]
-    float z_offset=-10f;
+    float z_offset=-10f; //offset for the camera in the z axis
     [SerializeField]
-    float y_offset=1f;
+    float y_offset=1f; //offset for the camera in the y axis
     [SerializeField]
     float smooth=0.3f;
     [SerializeField]
@@ -17,13 +17,15 @@ public class CameraBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player=GameObject.FindGameObjectWithTag("Player").transform;
+        player=GameObject.FindGameObjectWithTag("Player").transform; //this instruction connect the player in the game to this variable
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player!=null){
+        if(player!=null){ //this instrctions make the camera follow the player.
+        //if the player goes near the left or right border the camera stop follow to not go out of the background
+        //the third value of the vector3 stand for the smoothness with the camera follows the player
             if(player.position.x > -5.028 && player.position.x < 4.768){
                 targetPosition=new Vector3(player.position.x,player.position.y+y_offset,player.position.z+z_offset);
             }else{
