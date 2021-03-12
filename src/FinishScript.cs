fd54919c1f052e51bcef517ab0a4c6070bf95f1a
MicheleGiraudo73 <72200866+MicheleGiraudo73@users.noreply.github.com>
2021-03-12 08:53:42 +0100

    Update FinishScript.cs

--- src/FinishScript.cs
+++ src/FinishScript.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishScript : MonoBehaviour
 {
+    //these methods are associated with buttons, the menu method takes you to the menu scene and the restart method takes you to the start of the game.
     public void Menu(){
         ChangeScene(0);
     }
@@ -12,7 +13,7 @@ public class FinishScript : MonoBehaviour
     public void Restart(){
         ChangeScene(1);
     }
-   
+   //This method is used to change the scene
     void ChangeScene(int n){
      //The LoadScene method is used to start a scene
         SceneManager.LoadScene(n);
